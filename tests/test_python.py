1d4558951e6d97ac1642e8f88d1ba1747a31ce33
garciadeblas <gerardo.garciadeblas@telefonica.com>
2020-12-14 22:42:40 +0000

    Fixed warning in test_python.py: yaml.safe_load instead of load
    Change-Id: Ic324701eee6c808bee04690a41ad4e53cc68624a
    Signed-off-by: garciadeblas <gerardo.garciadeblas@telefonica.com>

--- tests/test_python.py
+++ tests/test_python.py
@@ -1,3 +1,18 @@
+#  Copyright 2020 ETSI
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+#  implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
 import osm_im.vnfd as vnfd_catalog
 from pyangbind.lib.serialise import pybindJSONDecoder
 import unittest
@@ -70,7 +85,7 @@ class PythonTest(unittest.TestCase):
         """
 
         try:
-            data = yaml.load(VNFD_YAML)
+            data = yaml.safe_load(VNFD_YAML)
 
             myvnfd = vnfd_catalog.vnfd()
             pybindJSONDecoder.load_ietf_json(data, None, None, obj=myvnfd)
